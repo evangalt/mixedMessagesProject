91b3f38f29278e4c25d8b3891bc9643c22086b5e
Evan Galt <evan.galt@gmail.com>
2023-04-21 23:21:40 -0500

    add new messages and update styling

--- js/main.js
+++ js/main.js
@@ -4,23 +4,38 @@ const cheerUpMessages = [
     "Don't worry, be happy!",
     "Every day is a new beginning. Take a deep breath and start again.",
     "You are stronger than you think!",
-    "The best way to predict your future is to create it!"
-  ];
+    "The best way to predict your future is to create it!",
+    "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+    "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+    "Happiness is not something ready made. It comes from your own actions.",
+    "Difficult roads often lead to beautiful destinations.",
+    "The only way to do great work is to love what you do."
+];
   
-  const motivationalMessages = [
+const motivationalMessages = [
     "You can do this!",
     "Believe in yourself and all that you are.",
-    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "Success is not final. Failure is not fatal. It is the courage to continue that counts.",
     "Happiness is not something ready made. It comes from your own actions.",
-    "Don't watch the clock; do what it does. Keep going."
-  ];
+    "Don't watch the clock; do what it does. Keep going.",
+    "Believe you can and you're halfway there.",
+    "Your limitation? It's only your imagination.",
+    "Push yourself, because no one else is going to do it for you.",
+    "If you want to achieve greatness stop asking for permission.",
+    "The harder you work for something, the greater you'll feel when you achieve it."
+];
   
-  const goodJobMessages = [
+const goodJobMessages = [
     "Great job! Keep up the good work!",
     "You're doing amazing!",
     "Your hard work is paying off!",
     "Success is not a destination, it's a journey. Keep going!",
-    "The only way to do great work is to love what you do."
+    "The only way to do great work is to love what you do.",
+    "You're making incredible progress!",
+    "I'm impressed by your dedication!",
+    "Keep pushing yourself, you're capable of amazing things!",
+    "Every small step counts towards big achievements!",
+    "Don't stop now, you're on a roll!"
   ];
   
   // Function to generate a random message based on the button clicked
